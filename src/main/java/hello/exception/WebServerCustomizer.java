753bf5c9e6a54da1d223e1a1573bcc9b9935d2f8
하지수 <dev.hajs@gmail.com>
2022-01-22 16:29:48 +0900

    API 예외 처리 시작 -. accept 헤더가 application/json 으로 들어오는 경우 html 오류 페이지가 아닌 json 에러 응답이 되도록 수정 개발

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,16 +6,16 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
 
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/templates/error-page/404");
-        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/templates/error-page/500");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
 
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/templates/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,17 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,14 +25,28 @@ public class ErrorPageController {
     public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
 
 
-    @RequestMapping("/templates/error-page/404")
+    @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
         printErrorInfo(request);
         return "error-page/404";
     }
 
-    @RequestMapping("/templates/error-page/500")
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+    }
+
+    @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
         printErrorInfo(request);
